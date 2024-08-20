1140e7588a92ad3cbffd0caac4b72d3d19a9aa56
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-08-20 17:59:35 +0300

    remvoe old email script

--- footer.php
+++ footer.php
@@ -68,15 +68,6 @@ window.addEventListener('load', disableLinksIfPatternInURLorHref);
 
 </script>
 
-<script>
-    (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
-        w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
-        m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://vip.maxiblocks.com/mtc.js','mt');
-
-    mt('send', 'pageview');
-</script>
-
 <style>
 	/* Standard scrollbars when hide-scrollbar class is present */
 @media (max-width: 1199px) {
