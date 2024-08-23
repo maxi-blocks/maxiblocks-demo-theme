15f3c5537f96db6d889e9d077c323b4c4b942dc8
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-08-23 13:01:44 +0300

    Add parts

--- js/maxi-blocks-editor.js
+++ js/maxi-blocks-editor.js
@@ -1,23 +1,49 @@
 wp.domReady(() => {
-    wp.hooks.addFilter(
-        'blocks.getSaveContent.extraProps',
-        'maxi-blocks/modify-image-urls',
-        (extraProps, blockType, attributes) => {
-            if (blockType.name !== 'core/image' && blockType.name !== 'core/gallery' && blockType.name !== 'maxi-blocks/image-maxi') {
-                return extraProps;
-            }
+  wp.hooks.addFilter(
+    'blocks.getSaveContent.extraProps',
+    'maxi-blocks/modify-image-urls',
+    (extraProps, blockType, attributes) => {
+      if (
+        blockType.name !== 'core/image'
+				&& blockType.name !== 'core/gallery'
+				&& blockType.name !== 'maxi-blocks/image-maxi'
+      ) {
+        return extraProps;
+      }
 
-            if (attributes.url) {
-                attributes.url = attributes.url.replace(
-                    'https://stagingdemo.maxiblocks.com/wp-content/uploads',
-                    'https://img.maxiblocks.com'
-                ).replace(
-                    'https://maxiblocks.com/demo/wp-content/uploads',
-                    'https://img.maxiblocks.com'
-                );
-            }
+      if (attributes.url) {
+        attributes.url = attributes.url
+          .replace(
+            'https://stagingdemo.maxiblocks.com/wp-content/uploads',
+            'https://img.maxiblocks.com',
+          )
+          .replace(
+            'https://maxiblocks.com/demo/wp-content/uploads',
+            'https://img.maxiblocks.com',
+          )
+          .replace(
+            'https://site-editor-demo.maxiblocks.com/wp-content/uploads',
+            'https://img.maxiblocks.com',
+          );
+      }
 
-            return extraProps;
-        }
-    );
+      if (attributes['dc-media-url']) {
+        attributes['dc-media-url'] = attributes['dc-media-url']
+          .replace(
+            'https://stagingdemo.maxiblocks.com/wp-content/uploads',
+            'https://img.maxiblocks.com',
+          )
+          .replace(
+            'https://maxiblocks.com/demo/wp-content/uploads',
+            'https://img.maxiblocks.com',
+          )
+          .replace(
+            'https://site-editor-demo.maxiblocks.com/wp-content/uploads',
+            'https://img.maxiblocks.com',
+          );
+      }
+
+      return extraProps;
+    },
+  );
 });
