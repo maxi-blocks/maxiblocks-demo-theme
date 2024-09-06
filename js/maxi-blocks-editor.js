747fd53cd4aa5bfa7bee9120e34c3a75998f94c5
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-09-06 16:34:36 +0300

    Fix console js errors

--- js/maxi-blocks-editor.js
+++ js/maxi-blocks-editor.js
@@ -1,49 +1,45 @@
-wp.domReady(() => {
-  wp.hooks.addFilter(
-    'blocks.getSaveContent.extraProps',
-    'maxi-blocks/modify-image-urls',
-    (extraProps, blockType, attributes) => {
-      if (
-        blockType.name !== 'core/image'
-				&& blockType.name !== 'core/gallery'
-				&& blockType.name !== 'maxi-blocks/image-maxi'
-      ) {
-        return extraProps;
-      }
+(function (wp) {
+  if (typeof wp !== 'undefined') {
+    wp.domReady(() => {
+      wp.hooks.addFilter(
+        'blocks.getSaveContent.extraProps',
+        'maxi-blocks/modify-image-urls',
+        (extraProps, blockType, attributes) => {
+          if (
+            blockType.name !== 'core/image'
+            && blockType.name !== 'core/gallery'
+            && blockType.name !== 'maxi-blocks/image-maxi'
+          ) {
+            return extraProps;
+          }
 
-      if (attributes.url) {
-        attributes.url = attributes.url
-          .replace(
-            'https://stagingdemo.maxiblocks.com/wp-content/uploads',
-            'https://img.maxiblocks.com',
-          )
-          .replace(
-            'https://maxiblocks.com/demo/wp-content/uploads',
-            'https://img.maxiblocks.com',
-          )
-          .replace(
-            'https://site-editor-demo.maxiblocks.com/wp-content/uploads',
-            'https://img.maxiblocks.com',
-          );
-      }
+          const newAttributes = { ...attributes };
 
-      if (attributes['dc-media-url']) {
-        attributes['dc-media-url'] = attributes['dc-media-url']
-          .replace(
-            'https://stagingdemo.maxiblocks.com/wp-content/uploads',
-            'https://img.maxiblocks.com',
-          )
-          .replace(
-            'https://maxiblocks.com/demo/wp-content/uploads',
-            'https://img.maxiblocks.com',
-          )
-          .replace(
-            'https://site-editor-demo.maxiblocks.com/wp-content/uploads',
-            'https://img.maxiblocks.com',
-          );
-      }
+          const replaceUrls = (url) => url
+            .replace(
+              'https://stagingdemo.maxiblocks.com/wp-content/uploads',
+              'https://img.maxiblocks.com',
+            )
+            .replace(
+              'https://maxiblocks.com/demo/wp-content/uploads',
+              'https://img.maxiblocks.com',
+            )
+            .replace(
+              'https://site-editor-demo.maxiblocks.com/wp-content/uploads',
+              'https://img.maxiblocks.com',
+            );
 
-      return extraProps;
-    },
-  );
-});
+          if (newAttributes.url) {
+            newAttributes.url = replaceUrls(newAttributes.url);
+          }
+
+          if (newAttributes['dc-media-url']) {
+            newAttributes['dc-media-url'] = replaceUrls(newAttributes['dc-media-url']);
+          }
+
+          return { ...extraProps, ...newAttributes };
+        },
+      );
+    });
+  }
+}(window.wp));
